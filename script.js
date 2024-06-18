cb5feb54a2137bcf807a9ab195ec5933ce9c6fc8
unknown <maxmilian.janda@infis.cz>
2024-06-18 12:11:43 +0200

    ScriptUpdate

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var player = 1;
-function appendSign(el) {
+var checkerOne = [];
+var checkerTwo = [];
+function appendSign(el, name) {
   if (player == 1) {
     document.getElementById(el).innerHTML = "O";
+    checkerOne.push(name);
+
+    console.log("Checker one " + checkerOne);
+    for (let i = 0; i <= checkerOne.length; i++) {}
     player = 2;
-    document.getElementById(el).disabled = true;
   } else {
+    checkerTwo.push(name);
+    console.log("Checker two " + checkerTwo);
     document.getElementById(el).innerHTML = "X";
     player = 1;
-    document.getElementById(el).disabled = true;
   }
 }
